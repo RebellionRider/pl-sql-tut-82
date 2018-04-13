a32bf5bc7c3075df31e8b7650e0bb5e928a83bad
Manish Sharma <32567588+RebellionRider@users.noreply.github.com>
2018-04-13 19:34:14 +0530

    Update DDL-with-Execute-Immediate-Script-2.sql

--- DDL-with-Execute-Immediate-Script-2.sql
+++ DDL-with-Execute-Immediate-Script-2.sql
@@ -1,5 +1,12 @@
-DECLARE
-    ddl_qry     VARCHAR2(150);
+/*
+    PL/SQL Tutorial 82: How to run DDL using Execute Immediate
+    YouTube tutorial link https://youtu.be/e1_hkvAyOxg
+    by Manish Sharma
+    Twitter.com/RebellionRider
+    facebook.com/TheRebellionRider
+*/
+
+SET SERVEROUTPUT ON;
 BEGIN
     EXECUTE IMMEDIATE 'CREATE TABLE tut_82(
                         tut_num     NUMBER(3),
